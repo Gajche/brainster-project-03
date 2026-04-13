9b528ed4a2e337fef300862d1d8615cbc654df55
Dejan Nikolovski <gajkax@gmail.com>
2026-04-13 11:43:56 +0200

    Switch to Mailtrap for email testing

--- app/Mail/ApplicationReviewed.php
+++ app/Mail/ApplicationReviewed.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ApplicationReviewed extends Mailable implements ShouldQueue
+class ApplicationReviewed extends Mailable
 {
 	use Queueable, SerializesModels;
 
